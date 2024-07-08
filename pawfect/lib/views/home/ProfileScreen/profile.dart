4b65e7ab0f5629f629685b393c2cc23213b9ef95
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-08 16:56:30 +0500

    Create profile.dart

---
+++ pawfect/lib/views/home/ProfileScreen/profile.dart
@@ -0,0 +1,47 @@
+import 'package:DogMatch/views/Auth/SigninOrSignUp/signUpsignIn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: user != null
+          ? Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      user.email.toString(),
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                   
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement sign out functionality
+                        FirebaseAuth.instance.signOut();
+                        Get.offAll(SignInSignUp());
+                      },
+                      child: Text('Sign Out'),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
