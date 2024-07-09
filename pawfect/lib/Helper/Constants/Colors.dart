88cde1a29496c1e15c1d27f1ee1208d7611928e1
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-09 16:52:42 +0500

    Added Colors Class

--- pawfect/lib/Helper/Constants/Colors.dart
+++ pawfect/lib/Helper/Constants/Colors.dart
@@ -2,8 +2,7 @@ import 'dart:ui';
 
 class KAppColors {
   
-  static const Color primaryColor = Color(0xFFFF5722);
-  static const Color secondaryColor = Color(0xFFF44336);
+
   static const Color tertiaryColor = Color(0xFFE91E63);
   static const Color quaternaryColor = Color(0xFF9C27B0);
   static const Color quinaryColor = Color(0xFF673AB7);
@@ -27,5 +26,27 @@ class KAppColors {
   static const Color trevigesimalColor = Color(0xFFAD1457);
   static const Color quattuorvigesimalColor = Color(0xFF9C27B0);
   static const Color quindevigesimalColor = Color(0xFF8E24AA);
+  //Color Pallet
+   static const Color primaryColor = Color(0xFFFF5722); // Deep Orange
+  static const Color secondaryColor = Color(0xFFF44336); // Red
+  static const Color accentColor = Color(0xFFFFC107); // Amber
+  static const Color backgroundColor = Color(0xFFFFFFFF); // White
+
+  static const Color lightPrimaryColor = Color(0xFFFF8A50);
+  static const Color mediumPrimaryColor = Color(0xFFFF5722);
+  static const Color darkPrimaryColor = Color(0xFFE64A19);
+
+  static const Color lightSecondaryColor = Color(0xFFFF7961);
+  static const Color mediumSecondaryColor = Color(0xFFF44336);
+  static const Color darkSecondaryColor = Color(0xFFD32F2F);
+
+  static const Color lightAccentColor = Color(0xFFFFECB3);
+  static const Color mediumAccentColor = Color(0xFFFFC107);
+  static const Color darkAccentColor = Color(0xFFFFA000);
+   // Button Colors
+  static const Color buttonColor = Color(0xFF4CAF50); // Green
+  static const Color lightButtonColor = Color(0xFF80E27E);
+  static const Color mediumButtonColor = Color(0xFF4CAF50);
+  static const Color darkButtonColor = Color(0xFF087F23);
 
 } 
