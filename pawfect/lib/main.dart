bca1bc9ef8de5e4da9911d7308b91a2f41ebc5e1
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-23 20:19:42 +0500

    Update main.dart

--- pawfect/lib/main.dart
+++ pawfect/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:DogMatch/Helper/Constants/Constants.dart';
+import 'package:DogMatch/Helper/LangController/langcontroller.dart';
 import 'package:DogMatch/views/Auth/Wrapper/authwrapper.dart';
 import 'package:DogMatch/views/SplashScreen/splashScreen.dart';
 import 'package:DogMatch/views/home/AddPostPage/add.dart';
@@ -5,49 +7,42 @@ import 'package:DogMatch/views/home/TabsPage/tabs_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_gen/gen_l10n/app_localization.dart';  
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-void main() async{
+import 'firebase_options.dart'; // Import the controller
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
+  final LocalizationController localizationController = Get.put(LocalizationController());
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: Locale('en'),
+      locale: Get.deviceLocale,
       debugShowCheckedModeBanner: false,
       title: 'DogMatch',
       theme: ThemeData(
         fontFamily: 'Popins',
-      
-        useMaterial3: true, 
-        
+        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         primarySwatch: Colors.deepOrange,
         brightness: Brightness.light,
       ),
-      // home: TabsPage(title: 'tinder',),
-    home: _buildHomePage(),
+      home: _buildHomePage(),
     );
-
-
-    
   }
-    Widget _buildHomePage() {
-    // Check if there is a current user logged in
-    User? currentUser = FirebaseAuth.instance.currentUser;
 
-    // Return either TabsPage or SplashScreen based on authentication state
+  Widget _buildHomePage() {
+    User? currentUser = FirebaseAuth.instance.currentUser;
     return currentUser != null ? TabsPage() : SplashScreen();
-   
   }
 }
