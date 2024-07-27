ebe7e4b48ad41c9712dfb7820a9d231045e55bfc
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-27 22:47:24 +0500

    Update splashScreen.dart

--- pawfect/lib/views/SplashScreen/splashScreen.dart
+++ pawfect/lib/views/SplashScreen/splashScreen.dart
@@ -1,3 +1,6 @@
+import 'package:DogMatch/Helper/Responsive/responsive.dart';
+import 'package:DogMatch/views/SplashScreen/SplashScreenDesktop.dart';
+import 'package:DogMatch/views/SplashScreen/splashScreenMobile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:DogMatch/views/Auth/SigninOrSignUp/signUpsignIn.dart';
@@ -14,102 +17,15 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
-      Get.to(SignInSignUp());
-    });
+    // Future.delayed(const Duration(seconds: 5), () {
+    //   Get.to(SignInSignUp());
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final isDesktop = constraints.maxWidth >= 600;
-          return Stack(
-            children: [
-              Container(
-                height: isDesktop ? constraints.maxHeight * 0.3 : constraints.maxHeight * 0.4,
-                margin: EdgeInsets.only(bottom: isDesktop ? 60 : 40),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    end: const Alignment(0.0, 0.4),
-                    begin: const Alignment(0.0, -1),
-                    colors: <Color>[Colors.pink, Color(0xFFFF5722)],
-                  ),
-                ),
-              ),
-              CustomPaint(
-                size: Size(
-                  constraints.maxWidth,
-                  isDesktop ? constraints.maxHeight * 0.31 : constraints.maxHeight * 0.4,
-                ),
-                painter: CurvedPainter(),
-              ),
-              Container(
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (isDesktop)
-                      SizedBox(
-                        height: constraints.maxHeight * 0.3,
-                      ),
-                    Container(
-                      width: constraints.maxWidth,
-                      height: isDesktop ? constraints.maxHeight * 0.4 - 100 : constraints.maxHeight * 0.3 - 100,
-                      child: Image.asset('assets/icons/gif/pets.gif'),
-                    ),
-                    Container(
-                      height: 75,
-                      width: constraints.maxWidth,
-                      child: Container(
-                        width: isDesktop ? 350.0 : 250.0,
-                        child: DefaultTextStyle(
-                          style: const TextStyle(
-                            fontSize: 30.0,
-                            fontFamily: 'Popins',
-                          ),
-                          child: Center(
-                            child: AnimatedTextKit(
-                              repeatForever: false,
-                              isRepeatingAnimation: false,
-                              animatedTexts: [
-                                TyperAnimatedText(
-                                  AppLocalizations.of(context)!.title,
-                                  textStyle: TextStyle(
-                                      color: Colors.deepOrange.shade400,
-                                      fontSize: isDesktop ? 45 : 35,
-                                      fontWeight: FontWeight.bold),
-                                  speed: const Duration(milliseconds: 300),
-                                ),
-                              ],
-                              onTap: () {
-                                print("Tap Event");
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: isDesktop ? 100 : 80,
-                    ),
-                    const CircularProgressIndicator(
-                        strokeWidth: 5,
-                        backgroundColor: Colors.deepOrange,
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                          Colors.white,
-                        )),
-                  ],
-                ),
-              ),
-            ],
-          );
-        },
-      ),
+      body: ResponsiveNess(mobile: SplashScreenMobile(), desktop: SplashScreenDesktop()),
     );
   }
 }
