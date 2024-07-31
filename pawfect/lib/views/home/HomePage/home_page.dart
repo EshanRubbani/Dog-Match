b9a46a5cdfe47a4bdfb016ff88ee27188fc3a949
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-07-31 13:34:34 +0500

    Create home_page.dart

---
+++ pawfect/lib/views/home/HomePage/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:DogMatch/Helper/Responsive/responsive.dart';
+import 'package:DogMatch/views/home/HomePage/homePageDesktop.dart';
+import 'package:DogMatch/views/home/HomePage/homePageMobile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ResponsiveNess(mobile: HomePageMobile(), desktop: const HomePageDesktop()),
+    );
+  }
+}
