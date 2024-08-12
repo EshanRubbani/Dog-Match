e2d74a9a5531c3e1930996e9cefa194ef8239bf7
Eshan Rubbani <eshanrubbani@gmail.com>
2024-08-12 12:28:15 +0500

    Update dpUpload.dart

--- pawfect/lib/views/Auth/ProfileImageUpload/dpUpload.dart
+++ pawfect/lib/views/Auth/ProfileImageUpload/dpUpload.dart
@@ -2,13 +2,21 @@ import 'package:DogMatch/Helper/Responsive/responsive.dart';
 import 'package:DogMatch/views/Auth/ProfileImageUpload/dpUploadDesktop.dart';
 import 'package:DogMatch/views/Auth/ProfileImageUpload/dpUploadMobile.dart';
 import 'package:flutter/material.dart';
+
 class DpUpload extends StatelessWidget {
-const DpUpload({ Key? key }) : super(key: key);
+  final String fname;
+  final String lname;
+
+  const DpUpload({Key? key, required this.fname, required this.lname})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: ResponsiveNess(mobile: DpUploadMobile(), desktop: DpUploadDesktop()),
+      body: ResponsiveNess(
+        mobile: DpUploadMobile(fname: fname, lname: lname),
+        desktop: DpUploadDesktop(),
+      ),
     );
   }
 }
