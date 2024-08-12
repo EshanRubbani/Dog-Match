afcdf60ff8973dfbf471f642cff6d950e5de1ef4
Eshan Rubbani <eshanrubbani@gmail.com>
2024-08-12 12:27:44 +0500

    Update homePageMobile.dart

--- pawfect/lib/views/home/HomePage/homePageMobile.dart
+++ pawfect/lib/views/home/HomePage/homePageMobile.dart
@@ -1,4 +1,5 @@
 import 'package:DogMatch/views/Auth/SigninOrSignUp/signUpsignIn.dart';
+import 'package:DogMatch/views/home/MatchPage/Match_Page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,7 +7,6 @@ import 'package:flutter_gen/gen_l10n/app_localization.dart'; // Import generated
 import 'package:DogMatch/views/home/DetailsPage/details_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../Helper/Painter/curved_painter.dart';
-import '../MatchPage/match_page.dart';
 
 class HomePageMobile extends StatefulWidget {
   @override
@@ -42,6 +42,7 @@ class _HomePageMobileState extends State<HomePageMobile> {
           'description': data['description'] as String,
           'interests': data['interests'] as String,
           'urls': urls,
+          'ownerID': data['owner'] as String,
         });
       }
     }
@@ -60,12 +61,12 @@ class _HomePageMobileState extends State<HomePageMobile> {
         children: [
           Container(
             height: MediaQuery.of(context).size.height * 0.3,
-            margin: EdgeInsets.only(bottom: 60),
+            margin: const EdgeInsets.only(bottom: 60),
             width: double.infinity,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               gradient: LinearGradient(
-                end: const Alignment(0.0, 0.4),
-                begin: const Alignment(0.0, -1),
+                end: Alignment(0.0, 0.4),
+                begin: Alignment(0.0, -1),
                 colors: <Color>[
                   Colors.pink,
                   Color(0xFFFF5722),
@@ -86,7 +87,7 @@ class _HomePageMobileState extends State<HomePageMobile> {
                 automaticallyImplyLeading: false,
                 elevation: 0,
                 backgroundColor: Colors.transparent,
-                actionsIconTheme: IconThemeData(opacity: 0.0),
+                actionsIconTheme: const IconThemeData(opacity: 0.0),
                 toolbarHeight: 56,
                 titleSpacing: 0,
                 centerTitle: false,
@@ -97,8 +98,9 @@ class _HomePageMobileState extends State<HomePageMobile> {
                     children: [
                       Text(
                         FirebaseAuth.instance.currentUser!.email.toString(),
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
+                          fontSize: 18,
                           color: Colors.white,
                         ),
                       ),
@@ -107,18 +109,21 @@ class _HomePageMobileState extends State<HomePageMobile> {
                           Container(
                             height: 42,
                             width: 42,
-                            padding: EdgeInsets.all(6),
+                            padding: const EdgeInsets.all(6),
                             decoration: BoxDecoration(
                               color: Colors.black.withOpacity(0.1),
                               borderRadius: BorderRadius.circular(100),
                             ),
-                            child: Icon(Icons.notifications_none_outlined, color: Colors.white),
+                            child: const Icon(
+                              Icons.notifications_none_outlined,
+                              color: Colors.white,
+                            ),
                           ),
-                          SizedBox(width: 5,),
+                          const SizedBox(width: 5),
                           Container(
                             height: 42,
                             width: 42,
-                            padding: EdgeInsets.all(6),
+                            padding: const EdgeInsets.all(6),
                             decoration: BoxDecoration(
                               color: Colors.black.withOpacity(0.1),
                               borderRadius: BorderRadius.circular(100),
@@ -128,7 +133,10 @@ class _HomePageMobileState extends State<HomePageMobile> {
                                 FirebaseAuth.instance.signOut();
                                 Get.offAll(SignInSignUp());
                               },
-                              child: Icon(Icons.login_outlined, color: Colors.white),
+                              child: const Icon(
+                                Icons.login_outlined,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ],
@@ -142,32 +150,45 @@ class _HomePageMobileState extends State<HomePageMobile> {
                   children: [
                     SizedBox(
                       height: MediaQuery.of(context).size.height * 0.7,
-                      child: PageView.builder(
+                     child: images.isEmpty
+                        ? Center(
+                            child: Text(
+                              "No posts yet",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey.shade600,
+                              ),
+                            ),
+                          )
+                        :PageView.builder(
                         itemCount: images.length,
                         itemBuilder: (context, index) {
                           return Padding(
-                            padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+                            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
                             child: AspectRatio(
                               aspectRatio: 0.7,
                               child: InkWell(
                                 onTap: () {
-                                  Get.to(DetailsPage(
-                                    Name: images[index]['name'],
-                                    age: images[index]['age'],
-                                    descripton: images[index]['description'],
-                                    urls: images[index]['urls'],
-                                    interests: images[index]['interests'],
-                                    image: images[index]['imageUrl'],
-                                  ),
+                                  Get.to(
+                                    DetailsPage(
+                                      Name: images[index]['name'],
+                                      age: images[index]['age'],
+                                      descripton: images[index]['description'],
+                                      urls: images[index]['urls'],
+                                      interests: images[index]['interests'],
+                                      image: images[index]['imageUrl'],
+                                      ownerID: images[index]['ownerID'],
+                                    ),
                                   );
-
-
                                 },
                                 child: ClipRRect(
                                   borderRadius: BorderRadius.circular(24),
-                                  child: Image.network(
-                                    images[index]['imageUrl'],
-                                    fit: BoxFit.cover,
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width * 0.9,
+                                    height: MediaQuery.of(context).size.height * 0.7,
+                                    color: Colors.grey.shade300,
+                                    child: buildCardDetails(context, index),
                                   ),
                                 ),
                               ),
@@ -181,85 +202,162 @@ class _HomePageMobileState extends State<HomePageMobile> {
               ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 16, 16, 104),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 64,
-                      width: 64,
-                      margin: EdgeInsets.fromLTRB(16, 16, 10, 16),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(100),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            spreadRadius: 0,
-                            blurRadius: 4,
-                            offset: Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.close_rounded,
-                          color: Colors.deepOrange,
-                          size: 28,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 64,
-                      width: 64,
-                      margin: EdgeInsets.fromLTRB(10, 16, 16, 16),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          end: const Alignment(0.0, 0.4),
-                          begin: const Alignment(0.0, -1),
-                          colors: <Color>[
-                            Colors.pink,
-                            Colors.deepOrange,
-                          ],
-                        ),
-                        borderRadius: BorderRadius.circular(100),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            spreadRadius: 0,
-                            blurRadius: 4,
-                            offset: Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MatchPage(),
-                            ),
-                          );
-                        },
-                        icon: Icon(
-                          Icons.favorite_rounded,
-                          color: Colors.white,
-                          size: 28,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          // Padding(
+          //   padding: const EdgeInsets.fromLTRB(16, 16, 16, 104),
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.end,
+          //     children: [
+          //       Row(
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: [
+          //           Container(
+          //             height: 64,
+          //             width: 64,
+          //             margin: const EdgeInsets.fromLTRB(16, 16, 10, 16),
+          //             decoration: BoxDecoration(
+          //               color: Colors.white,
+          //               borderRadius: BorderRadius.circular(100),
+          //               boxShadow: [
+          //                 BoxShadow(
+          //                   color: Colors.black.withOpacity(0.2),
+          //                   spreadRadius: 0,
+          //                   blurRadius: 4,
+          //                   offset: const Offset(0, 4),
+          //                 ),
+          //               ],
+          //             ),
+          //             child: IconButton(
+          //               onPressed: () {},
+          //               icon: const Icon(
+          //                 Icons.close_rounded,
+          //                 color: Colors.deepOrange,
+          //                 size: 28,
+          //               ),
+          //             ),
+          //           ),
+          //           Container(
+          //             height: 64,
+          //             width: 64,
+          //             margin: const EdgeInsets.fromLTRB(10, 16, 16, 16),
+          //             decoration: BoxDecoration(
+          //               gradient: const LinearGradient(
+          //                 end: Alignment(0.0, 0.4),
+          //                 begin: Alignment(0.0, -1),
+          //                 colors: <Color>[
+          //                   Colors.pink,
+          //                   Colors.deepOrange,
+          //                 ],
+          //               ),
+          //               borderRadius: BorderRadius.circular(100),
+          //               boxShadow: [
+          //                 BoxShadow(
+          //                   color: Colors.black.withOpacity(0.2),
+          //                   spreadRadius: 0,
+          //                   blurRadius: 4,
+          //                   offset: const Offset(0, 4),
+          //                 ),
+          //               ],
+          //             ),
+          //             child: IconButton(
+          //               onPressed: () {
+          //                 final currentImage = images[indexPage];
+          //                 Get.to(
+          //                   MatchPage(
+          //                     name: currentImage['name'],
+          //                     age: currentImage['age'],
+          //                     description: currentImage['description'],
+          //                     interests: currentImage['interests'],
+          //                     image: currentImage['imageUrl'],
+          //                     urls: currentImage['urls'],
+          //                     ownerID: currentImage['ownerID'],
+          //                   ),
+          //                 );
+          //               },
+          //               icon: const Icon(
+          //                 Icons.favorite_rounded,
+          //                 color: Colors.white,
+          //                 size: 28,
+          //               ),
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
+    );
+  }
+
+  Column buildCardDetails(BuildContext context, int index) {
+    return Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.7 - 150,
+          child: Image.network(
+            images[index]['imageUrl'],
+            fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          children: [
+            const SizedBox(width: 25),
+            const Icon(Icons.pets_outlined, color: Colors.grey, size: 25),
+            const SizedBox(width: 5),
+            Text(
+              images[index]['name'],
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Lato",
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.start,
+            ),
+            const Text(
+              " , ",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "lato",
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              images[index]['age'],
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "lato",
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const SizedBox(width: 25),
+            Container(
+              width: 365,
+              height: 49,
+              child: Text(
+                images[index]['description'],
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: "Poppins",
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
