4601c10bc2989e63b221eda15d1d737e6ecb15ef
Eshan Rubbani <eshanrubbani@gmail.com>
2024-08-12 12:27:56 +0500

    Update profile.dart

--- pawfect/lib/views/home/ProfileScreen/profile.dart
+++ pawfect/lib/views/home/ProfileScreen/profile.dart
@@ -1,62 +1,16 @@
-import 'package:DogMatch/views/Auth/SigninOrSignUp/signUpsignIn.dart';
-import 'package:DogMatch/views/home/AddPostPage/add.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:DogMatch/Helper/Responsive/responsive.dart';
+import 'package:DogMatch/views/home/ProfileScreen/profileDesktop.dart';
+import 'package:DogMatch/views/home/ProfileScreen/profileMobile.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class ProfileScreen extends StatelessWidget {
+class Profile extends StatelessWidget {
+ Profile({ Key? key }) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    User? user = FirebaseAuth.instance.currentUser;
-
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: user != null
-          ? Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundImage:
-                    NetworkImage(user.photoURL!),
-                                   radius: 100,
-              ),
-              const SizedBox(height: 30),
-              Text(
-                user.displayName ?? 'No Name',
-                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                user.email ?? 'No Email',
-                style: const TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Get.to(const Add());
-                },
-                child: const Text('Add Post'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Get.offAll(SignInSignUp());
-                },
-                child: const Text('Sign Out'),
-              ),
-            ],
-          ),
-        ),
-      )
-          : const Center(
-        child: CircularProgressIndicator(),
+      body:  ResponsiveNess(
+        mobile: ProfileScreenMobile(), desktop: ProfileScreenDesktop(),
       ),
     );
   }
